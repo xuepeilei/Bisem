1b99a5ecc31f921a5f4a69ffd7ba25db8c399312
xuepeilei <xuepeilei@163.com>
2017-04-14 18:39:25 +0800

    标记模块
    最终结果呈现于网页上

--- correction/mark.py
+++ correction/mark.py
@@ -6,9 +6,11 @@ Created on 2017年4月14日
 标记模块
 '''
 def mark(sentence,x,y):
-    with open("e:/test.html","w") as w:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-        html="<html>%s</br></html>"%sentence
+    mark_x='<font color="#FF0000">'+x+'</font>'
+    mark_y='<font color="#FF0000">'+y+'</font>'
+    sentence_x=sentence.replace(x,mark_x)
+    sentence_y=sentence_x.replace(y,mark_y)
+    
+    with open("e:/test.html","w") as w:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+        html="<html>%s</html>"%sentence_y
         w.write(html)
-    
-    
