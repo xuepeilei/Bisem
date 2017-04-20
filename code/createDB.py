2498eaff6e981b6611442617c383f6e7d077e469
xuepeilei <xuepeilei@163.com>
2017-04-20 17:57:46 +0800

    设置环境变量

--- code/createDB.py
+++ code/createDB.py
@@ -4,11 +4,14 @@ Created on 2017年4月20日
 
 @author: xue
 '''
+import os
 from DB.batch import *
 from DB.sensesDB import *
 from DB.sememeDB import *
 
 if __name__ == "__main__":
+    export_path="export PYTHONPATH=$PYTHONPATH:/home/xue/Bisem/code/"
+    os.system(export_path)
     batch("sigram",1)
     batch("bigram",2)
     senses("./../corpus/hownet.txt")
