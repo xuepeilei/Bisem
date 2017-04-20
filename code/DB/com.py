f43bffcdfa326a55fe2362b4f35081b17f56465c
xuepeilei <xuepeilei@163.com>
2017-04-20 12:14:50 +0800

    总控程序

--- code/DB/com.py
+++ code/DB/com.py
@@ -12,8 +12,8 @@ connect = pymysql.Connect(
     host='localhost',
     port=3306,
     user='root',
-    passwd='',
-    db='test',
+    passwd='******',
+    db='bisem',
     charset='utf8'
 )
 
--- code/createDB.py
+++ code/createDB.py
@@ -11,5 +11,5 @@ from DB.sememeDB import *
 if __name__ == "__main__":
     batch("sigram",1)
     batch("bigram",2)
-    senses("./../../corpus/hownet.txt")
+    senses("./../corpus/hownet.txt")
     sememe()
