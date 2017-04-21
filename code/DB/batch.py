c10ff737be4b6ef86282b5cc8676ac7f73a8c6c4
xuepeilei <xuepeilei@163.com>
2017-04-21 18:40:18 +0800

    修正执行路径

--- code/DB/batch.py
+++ code/DB/batch.py
@@ -1,4 +1,3 @@
-# -*- coding: UTF-8 -*- 
 '''
 Created on 2017年4月19日
 
@@ -12,29 +11,29 @@ from DB.ngramDB import *
 
 
 def batch(address,n):
-#列出路径下文件
+#corpus_path为语料库路径
     corpus_path=os.path.join(os.getcwd(),"../corpus/")
     corpus_path=os.path.join(corpus_path,address)
 
     corpus_list=os.listdir(corpus_path)
     
     for i in corpus_list:
-        #找到csv文件路径
-        exe_path=corpus_path+"/"+i
-
+        #zip_path为zip包路径，csv_path为csv文件路径
+        zip_path=corpus_path+"/"+i
+        csv_path=os.path.splitext(zip_path)[0]
         #判断操作系统
         if platform.system()=="Windows":
-            exe_unzip="expand "+exe_path
-            exe_del="del "+exe_path
+            exe_unzip="expand "+zip_path
+            exe_del="del "+csv_path
         elif platform.system()=="Linux":
-            exe_unzip="unzip "+exe_path+" -d "+corpus_path
-            exe_del="rm -rf "+exe_path
+            exe_unzip="unzip "+zip_path+" -d "+corpus_path
+            exe_del="rm -rf "+csv_path
             
         #解压zip文件
         os.system(exe_unzip)
         
         #执行重构
-        reconfiguration(exe_path,n)
+        reconfiguration(csv_path,n)
           
         #删除csv文件
         os.system(exe_del)
