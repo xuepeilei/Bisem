b0fe852c766efa39d429dd395fd70b1d5dee3d93
Administrator <Administrator@AFAAW-704241638>
2017-04-26 23:22:57 +0800

    测试数据集

--- code/test.py
+++ code/test.py
@@ -5,6 +5,6 @@ from Proofread.corr import *
 with open("/home/xue/Bisem/corpus/test.txt","r") as r:
     for line in r.readlines():
         corr_sentence=corr(line)
-        with open("/home/xue/Bisem/corpus/result","a") as a:
+        with open("/home/xue/Bisem/corpus/result.html","a") as a:
             sentence='<p>'+corr_sentence+'</p>'
             a.write(sentence)
